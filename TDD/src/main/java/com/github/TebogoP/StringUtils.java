e8952d64f40952c37c1b5dbec4a3bf70a3af8338
Tebogo <tphiri.firi@gmail.com>
2026-06-29 11:11:19 +0200

    Feat: REFACTOR Step 3: REFACTOR - added other tests for reverseEmptyWord(), reverseSingleChar  isHelloPalindrome() to drive the next feature

--- TDD/src/main/java/com/github/TebogoP/StringUtils.java
+++ TDD/src/main/java/com/github/TebogoP/StringUtils.java
@@ -3,11 +3,16 @@ package com.github.TebogoP;
 public class StringUtils {
 
     public String reverse(String word) {
+
         String charWord[] = word.split("");
         String reversedWord = "";
         for(int i = charWord.length -1 ; i >= 0 ; i -- ){
             reversedWord += charWord[i];
         }
         return reversedWord;
+    }
+
+    public boolean isPalindrome(String input, String result) {
+        return input.equals(result);
     }
 }
--- TDD/src/test/java/StringUtilsTest.java
+++ TDD/src/test/java/StringUtilsTest.java
@@ -2,6 +2,7 @@ import com.github.TebogoP.StringUtils;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class StringUtilsTest {
     @Test
@@ -10,5 +11,30 @@ public class StringUtilsTest {
         StringUtils stringUtilsObj = new StringUtils();
         String result  = stringUtilsObj.reverse(word);
         assertEquals("olleh", result);
+    }
+
+    @Test
+    public void reverseEmptyWord(){
+        String word = " ";
+        StringUtils stringUtilsObj = new StringUtils();
+        String result  = stringUtilsObj.reverse(word);
+        assertEquals(" ", result);
+    }
+
+    @Test
+    public void reverseSingleChar(){
+        String word = "a";
+        StringUtils stringUtilsObj = new StringUtils();
+        String result  = stringUtilsObj.reverse(word);
+        assertEquals("a", result);
+
+    }
+
+    @Test
+    public void isHelloPalindrome(){
+        String word = "hello";
+        StringUtils stringUtilsObj = new StringUtils();
+        String result  = stringUtilsObj.reverse(word);
+        assertFalse(stringUtilsObj.isPalindrome(word,result));
     }
 }
