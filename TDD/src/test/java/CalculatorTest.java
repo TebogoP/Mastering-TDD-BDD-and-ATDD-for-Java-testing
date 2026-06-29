a77e6670c06a9fac4cc249a81b5cb0f860e39fd9
Tebogo <tphiri.firi@gmail.com>
2026-06-29 10:28:12 +0200

    Feat: RED Step1: RED - wrote a failing test.            - Class Calculator doesn't exist            - Method add(int, int) doesn't exist

--- TDD/src/test/java/CalculatorTest.java
+++ TDD/src/test/java/CalculatorTest.java
@@ -2,5 +2,16 @@
 ** with method that adds two positive numbers
 * */
 
+import com.github.TebogoP.Calculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorTest {
+
+    @Test
+    void addTwoPositiveNumbers() {
+        Calculator calc = new Calculator();  // doesn't exist yet!
+        int result = calc.add(3, 5);
+        assertEquals(8, result);
+    }
 }
